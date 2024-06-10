a9c0bd4e573b99a21210ed85b7ba84d380414664
unknown <jacob.a.gerales@gmail.com>
2024-06-10 14:33:08 -0700

    Contact me functional. Message being sent to email

--- application/app.js
+++ application/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
 const app = express();
 const port = 3000;
 
@@ -12,14 +15,43 @@ app.set('views', path.join(__dirname, 'public', 'views'));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware for parsing JSON
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // Route for the home page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 // Route for About Me
-app.get('/contactMe', (req, res) => {
-  res.render('contactMe');
+app.post('/contactMe', async (req, res) => {
+  const {name, email, message} = req.body;
+
+  // need to use ENV file
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'jacob.a.gerales@gmail.com',
+      pass: 'itiy slwn nbcp qidw'
+    }
+  });
+
+  const mailOptions = {
+    from: email, // Sender address
+    to: 'jacob.a.gerales@gmail.com', // List of recipients
+    subject: 'New Contact Form Submission', // Subject line
+    text: `You have received a new message from ${name} (${email}):\n\n${message}` // Plain text body
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Message sent successfully");
+    res.render('index');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending message.');
+  }
 });
 
 // Route for Projects
