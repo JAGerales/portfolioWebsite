b081cc8af85d6787797184dd9709763a873e949d
unknown <jacob.a.gerales@gmail.com>
2024-06-10 18:35:22 -0700

    Chat bot added! WIP with configuring prompt engineering and overall polish

--- application/app.js
+++ application/app.js
@@ -1,7 +1,12 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
+const OpenAI = require('openai');
 
 const app = express();
 const port = 3000;
@@ -16,32 +21,38 @@ app.set('views', path.join(__dirname, 'public', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware for parsing JSON
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+// Configure Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// chatGPT API Configuration
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_API_KEY
+});
 
 // Route for the home page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Route for About Me
+// Route for Contact Me
 app.post('/contactMe', async (req, res) => {
-  const {name, email, message} = req.body;
-
-  // need to use ENV file
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'jacob.a.gerales@gmail.com',
-      pass: 'itiy slwn nbcp qidw'
-    }
-  });
+  const { name, email, message } = req.body;
 
   const mailOptions = {
     from: email, // Sender address
-    to: 'jacob.a.gerales@gmail.com', // List of recipients
+    to: process.env.EMAIL_USER, // List of recipients
     subject: 'New Contact Form Submission', // Subject line
-    text: `You have received a new message from ${name} (${email}):\n\n${message}` // Plain text body
+    text: `You have received a new message from ${name} (${email}):\n\n${message}`, // Plain text body
   };
 
   try {
@@ -51,6 +62,30 @@ app.post('/contactMe', async (req, res) => {
   } catch (error) {
     console.error('Error sending email:', error);
     res.status(500).send('Error sending message.');
+  }
+});
+
+app.post('/chat', async (req, res) => {
+  const { message } = req.body;
+
+  try{
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {"role": "system", "content": `You are a helpful chatbot that mimics Jacob Gerales' speech patterns and knowledge. You are hosted on his portfolio website and you want to impress those that interact with his page.
+        Jacob is friendly, likes to sometimes drop bay area slang, and likes to write messages in lower case. 
+        He also has a unique habit of using b/c instead of because as well as traditional internet slang such as lol, bro, and such. he uses slang sparingly though
+        He has computer science knowledge at a bachelor level, powerlifts, takes photos with a FUJIFILM X-T10, and plays video games such as League of Legends and Rainbow Six Seige
+        He is a second generation filipino american, 24 years old, and is looking for a job`},
+        {"role": "user", "content": message}]
+    });
+    console.log(chatCompletion.choices[0].message);
+    const botMessage = chatCompletion.choices[0].message;
+    console.log(botMessage.content);
+    res.json({ reply: botMessage.content });
+  } catch (error){
+    console.log('Error: ', error);
+    res.status(500).send('Error interacting with chatbot');
   }
 });
 
